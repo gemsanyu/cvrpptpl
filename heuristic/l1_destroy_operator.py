a435152f96177a760f125dc98264a3b29b4d4955
gemilang <git.gemilang.santiyuda@gmail.com>
2025-03-10 20:58:12 +0800

    add worst removal

--- heuristic/l1_destroy_operator.py
+++ heuristic/l1_destroy_operator.py
@@ -21,21 +21,41 @@ class RandomDestinationsRemoval(Operator):
         for dest_idx in dests_to_remove:
             remove_a_destination(solution, dest_idx)
              
-# class WorstDestinationsRemoval(Operator):
-#     def __init__(self, min_to_remove, max_to_remove):
-#         super().__init__()
-#         self.min_to_remove = min_to_remove
-#         self.max_to_remove = max_to_remove
+class WorstDestinationsRemoval(Operator):
+    def __init__(self, min_to_remove, max_to_remove):
+        super().__init__()
+        self.min_to_remove = min_to_remove
+        self.max_to_remove = max_to_remove
     
-#     def apply(self, problem, solution):
-#         dests_in_routes = np.where(solution.destination_vehicle_assignmests > -1)[0]
-#         num_to_remove = randint(self.min_to_remove, self.max_to_remove)
-#         min(num_to_remove, len(dests_in_routes))
-#         # pick randomly
-#         dests_to_remove = np.random.choice(dests_in_routes, num_to_remove, replace=False)
-#         for dest_idx in dests_to_remove:
-#              remove_a_destination(solution, dest_idx)
-        
+    def apply(self, problem, solution):
+        dests_in_routes = np.where(solution.destination_vehicle_assignmests > -1)[0]
+        removal_d_costs = compute_removal_d_costs(problem, solution, dests_in_routes)
+        sorted_idx = np.argsort(removal_d_costs)
+        dests_in_routes = dests_in_routes[sorted_idx]
+
+        num_to_remove = randint(self.min_to_remove, self.max_to_remove)
+        num_to_remove = min(num_to_remove, len(dests_in_routes))
+        dests_in_routes = dests_in_routes[:num_to_remove]
+        # pick randomly
+        dests_to_remove = np.random.choice(dests_in_routes, num_to_remove, replace=False)
+        for dest_idx in dests_to_remove:
+             remove_a_destination(solution, dest_idx)
+
+def compute_removal_d_costs(problem:Cvrpptpl, 
+                           solution: Solution, 
+                           dests_in_routes: np.ndarray):
+    removal_d_costs = np.zeros_like(dests_in_routes, dtype=float)
+    for i, dest_idx in enumerate(dests_in_routes):
+        v_idx = solution.destination_vehicle_assignmests[dest_idx]
+        pos = solution.routes[v_idx].index(dest_idx)
+        prev_dest_idx = solution.routes[v_idx][pos-1]
+        next_dest_idx = solution.routes[v_idx][(pos+1)%len(solution.routes[v_idx])]
+        related_arc_costs = problem.distance_matrix[[prev_dest_idx, dest_idx, prev_dest_idx],[dest_idx, next_dest_idx, next_dest_idx]]*problem.vehicle_costs[v_idx]
+        prev_to_dest_cost, pos_to_dest_cost, prev_to_next_cost = related_arc_costs
+        d_cost = prev_to_next_cost -(prev_to_dest_cost + pos_to_dest_cost)
+        removal_d_costs[i] = d_cost
+    return removal_d_costs
+    
         
 class RandomRouteSegmentRemoval(Operator):
     # Level 1 Operator
@@ -63,11 +83,9 @@ def remove_a_destination(solution: Solution,
     prev_dest_idx = solution.routes[v_idx][pos-1]
     next_dest_idx = solution.routes[v_idx][(pos+1)%len(solution.routes[v_idx])]
     
-    # print(pos)
     related_arc_costs = problem.distance_matrix[[prev_dest_idx, dest_idx, prev_dest_idx],[dest_idx, next_dest_idx, next_dest_idx]]*problem.vehicle_costs[v_idx]
     prev_to_dest_cost, pos_to_dest_cost, prev_to_next_cost = related_arc_costs
     d_cost = prev_to_next_cost -(prev_to_dest_cost + pos_to_dest_cost)
-    # print(d_cost)
     solution.total_vehicle_charge = solution.total_vehicle_charge + d_cost
     # remove from route
     solution.destination_vehicle_assignmests[dest_idx] = -1
