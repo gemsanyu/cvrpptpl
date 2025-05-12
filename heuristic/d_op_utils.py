some bug in travel cost computation

heuristic/d_op_utils.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from problem.cvrpptpl import Cvrpptpl
@@ -16,7 +18,7 @@ def complete_customers_removal(problem: Cvrpptpl, solution: Solution, custs_idx:
         # more complicated
         # remove demand from locker
         solution.locker_loads[dest_idx] -= demand
-        solution.total_locker_charge -= solution.locker_costs[dest_idx]*demand
+        solution.total_locker_charge -= solution.locker_costs[dest_idx]
         solution.package_destinations[cust_idx] = NO_DESTINATION
         # remove from mrt line
         incoming_mrt_line_idx = solution.incoming_mrt_lines_idx[dest_idx]
@@ -68,7 +70,7 @@ def compute_customer_removal_d_costs(problem: Cvrpptpl, solution: Solution):
         # if the locker use mrt line
         # also compute mrt line usage cost (demand-related)
         locker_idx = solution.package_destinations[cust_idx]
-        locker_load_cost = demand*solution.locker_costs[locker_idx]
+        locker_load_cost = solution.locker_costs[locker_idx]
         cust_d_costs[cust_idx] += locker_load_cost
         incoming_mrt_line_idx = solution.incoming_mrt_lines_idx[locker_idx]
         is_using_mrt = incoming_mrt_line_idx is not None and solution.mrt_usage_masks[incoming_mrt_line_idx]
@@ -103,9 +105,12 @@ def remove_a_destination(solution: Solution,
     prev_dest_idx = solution.routes[v_idx][pos-1]
     next_dest_idx = solution.routes[v_idx][(pos+1)%len(solution.routes[v_idx])]
     
+    
     related_arc_costs = problem.distance_matrix[[prev_dest_idx, dest_idx, prev_dest_idx],[dest_idx, next_dest_idx, next_dest_idx]]*problem.vehicle_costs[v_idx]
     prev_to_dest_cost, pos_to_dest_cost, prev_to_next_cost = related_arc_costs
     d_cost = prev_to_next_cost -(prev_to_dest_cost + pos_to_dest_cost)
+    
+    
     solution.total_vehicle_charge = solution.total_vehicle_charge + d_cost
     # remove from route
     solution.destination_vehicle_assignmests[dest_idx] = NO_VEHICLE
@@ -135,14 +140,13 @@ def remove_segment(solution: Solution,
     problem = solution.problem
     dests_to_remove = solution.routes[vehicle_idx][start_idx:end_idx]
     # adjust vehicle charge
-    start_dest_idx, end_dest_idx = dests_to_remove[0], dests_to_remove[-1]
     prev_dest_idx = solution.routes[vehicle_idx][start_idx-1]
     next_dest_idx = solution.routes[vehicle_idx][end_idx%len(solution.routes[vehicle_idx])]
-    vehicle_costs = problem.distance_matrix[[prev_dest_idx, end_dest_idx, prev_dest_idx],[start_dest_idx, next_dest_idx, next_dest_idx]]*problem.vehicle_costs[vehicle_idx]
-    prev_to_start_cost, end_to_next_cost, prev_to_next_cost = vehicle_costs
-    solution.total_vehicle_charge = solution.total_vehicle_charge - (prev_to_start_cost + end_to_next_cost) + prev_to_next_cost
-    # remove from route
+    r = [prev_dest_idx] + dests_to_remove + [next_dest_idx]
+    d_cost = -problem.distance_matrix[r[:-1], r[1:]].sum()*problem.vehicle_costs[vehicle_idx] + problem.distance_matrix[prev_dest_idx, next_dest_idx]
+    solution.total_vehicle_charge = solution.total_vehicle_charge + d_cost
     solution.destination_vehicle_assignmests[dests_to_remove] = NO_VEHICLE
+    # remove from route
     solution.routes[vehicle_idx] = solution.routes[vehicle_idx][:start_idx] + solution.routes[vehicle_idx][end_idx:]
     solution.vehicle_loads[vehicle_idx] -= np.sum(solution.destination_total_demands[dests_to_remove])
 

heuristic/random_initialization.py
@@ -22,7 +22,7 @@ def first_fit_destination_assignment(node_idx: int, problem: Cvrpptpl, solution:
             if feasible_assignment_found:
                 # commit other changes
                 solution.package_destinations[node_idx]=dest_idx
-                solution.total_locker_charge += problem.demands[node_idx]*problem.locker_costs[dest_idx]
+                solution.total_locker_charge += problem.locker_costs[dest_idx]
                 return True
             else:
                 # revert load change
@@ -75,7 +75,7 @@ def greedy_route_insertion(problem: Cvrpptpl, solution: Solution):
     # sort them based on their distance to depot
     required_destinations_distance_to_depot = problem.distance_matrix[required_destinations, 0]
     sorted_idx = np.argsort(required_destinations_distance_to_depot)
-    required_destinations = required_destinations[sorted_idx]
+    required_destinations = required_destinations[sorted_idx].tolist()
     # now we know no-one is in route yet, so no further checking needed
     vehicle_current_locations: np.ndarray = np.zeros([problem.num_vehicles,], dtype=int)
     for dest_idx in required_destinations:

problem/cvrpptpl.py
@@ -56,7 +56,7 @@ class Cvrpptpl:
         self.mrt_line_stations_idx: np.ndarray = np.empty([len(mrt_lines),2], dtype=int)
         for i, mrt_line in enumerate(mrt_lines):
             self.mrt_line_stations_idx[i, :] = (mrt_line.start_station.idx, mrt_line.end_station.idx)        
-        self.incoming_mrt_lines_idx: List[int] = [999999 for _ in range(self.num_nodes)]
+        self.incoming_mrt_lines_idx: List[int] = [None for _ in range(self.num_nodes)]
         for i, mrt_line in enumerate(mrt_lines):
             self.incoming_mrt_lines_idx[mrt_line.end_station.idx] = i
         self.vehicle_capacities: np.ndarray = np.asanyarray([vehicle.capacity for vehicle in self.vehicles], dtype=int)